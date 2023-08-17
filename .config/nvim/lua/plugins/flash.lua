37f32ef30bdebcf792b96589384afb48b94853f6
Nadav Hammer <twindata36@gmail.com>
2023-08-17 12:54:33 +0300

    feat: only search start of words when using flash

---
+++ .config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/flash.nvim",
+  keys = {
+    {
+      "/",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump({
+          search = {
+            mode = function(str)
+              return "\\<" .. str
+            end,
+          },
+        })
+      end,
+      desc = "Flash",
+    },
+  },
+}
