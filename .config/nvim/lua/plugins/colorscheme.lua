d8e2ad7ea686751460b4de6a701489843e09a255
Nadav Hammer <twindata36@gmail.com>
2023-08-17 12:50:11 +0300

    refactor: optimize colorscheme selection

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,8 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", lazy = "false", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = function()
-        require("catppuccin").load()
-      end,
+      colorscheme = "catppuccin",
     },
   },
 }
