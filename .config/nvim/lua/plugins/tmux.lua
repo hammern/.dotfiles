6fc66021b8f6f015409de080e30619ec2c567b3c
Nadav Hammer <twindata36@gmail.com>
2023-08-27 11:20:36 +0300

    fix: neovim to tmux navigation

--- .config/nvim/lua/plugins/tmux.lua
+++ .config/nvim/lua/plugins/tmux.lua
@@ -1,6 +1,5 @@
 return {
   "christoomey/vim-tmux-navigator",
-  lazy = false,
   keys = {
     { "<C-h>", "<cmd> TmuxNavigateLeft<CR>", desc = "window left" },
     { "<C-l>", "<cmd> TmuxNavigateRight<CR>", desc = "window right" },
