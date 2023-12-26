62e82b1b613d89d831b157d6511dec5028a1d608
Nadav Hammer <twindata36@gmail.com>
2023-12-26 14:54:27 +0200

    feat: tokyonight theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 return {
-	color_scheme = "Catppuccin Mocha",
+	color_scheme = "tokyonight_night",
 	enable_tab_bar = false,
 	window_decorations = "RESIZE",
 	window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
