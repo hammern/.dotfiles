4328f01113e4b191612bd6244786bfb11dc74561
Nadav Hammer <twindata36@gmail.com>
2023-12-31 21:40:02 +0200

    feat: add oil.nvim for better filesystem editing

--- .config/nvim/ftplugin/netrw.vim
+++
@@ -1,3 +0,0 @@
-" Override <C-l> of netrw to move between tmux panes
-" https://github.com/christoomey/vim-tmux-navigator/issues/189
-nnoremap <buffer> <C-l> :TmuxNavigateRight<cr>
---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,12 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+    keymaps = {
+      ["C-l"] = false, -- Fix collision with vim-tmux-navigator
+    },
+  },
+}
