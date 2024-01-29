a470a95161aaf29be926b6e43c7d4e044867868a
Nadav Hammer <twindata36@gmail.com>
2024-01-29 19:14:57 +0200

    feat: github copilot nvim plugin

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,12 +2,18 @@ return {
   -- Set lualine as statusline
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "AndreM222/copilot-lualine",
+  },
   opts = {
     options = {
       theme = "tokyonight",
       component_separators = "|",
       section_separators = "",
+    },
+    sections = {
+      lualine_x = { "copilot", "encoding", "fileformat", "filetype" },
     },
     extensions = { "oil" },
   },
