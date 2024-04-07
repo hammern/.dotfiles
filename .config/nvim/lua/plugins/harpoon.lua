96541c76ce6f1bc5f348359737fa1da9805e47ef
Nadav Hammer <twindata36@gmail.com>
2024-04-07 10:31:28 +0300

    fix: harpoon append to add

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -9,7 +9,7 @@ return {
     {
       "<leader>a",
       function()
-        require("harpoon"):list():append()
+        require("harpoon"):list():add()
       end,
       desc = "[A]ppend to Harpoon",
     },
