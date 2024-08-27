b1e00d63e476045a03b2e6b312ca0855ee5b41ec
Nadav Hammer <twindata36@gmail.com>
2024-08-27 11:07:37 +0300

    feat: change git remote on installation

--- install/install.sh
+++ install/install.sh
@@ -1,13 +1,17 @@
 DOTFILES_DIR="/home/hammern/.dotfiles"
 
-git clone https://github.com/hammern/.dotfiles "$DOTFILES_DIR"
-
 hostname=$(fzf --height=8 --border --border-label="Select a hostname" <$DOTFILES_DIR/install/hostnames.txt)
 
 if [[ -z $hostname ]]; then
 	echo "Hostname not selected, exiting..."
 	exit 0
 fi
+
+git clone https://github.com/hammern/.dotfiles "$DOTFILES_DIR"
+
+cd "$DOTFILES_DIR"
+git remote rm origin
+git remote add origin git@github.com:hammern/.dotfiles.git
 
 nixos-generate-config --show-hardware-config >"$DOTFILES_DIR"/hosts/"$hostname"/hardware-configuration.nix
 
