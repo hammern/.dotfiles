d01087e877a87163569c2b893351cc8876ac7c66
Nadav Hammer <twindata36@gmail.com>
2024-12-27 15:48:10 +0200

    refactor: move which-key key chains to opts

--- modules/home-manager/nvim/nvim/lua/plugins/which-key.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/which-key.lua
@@ -2,14 +2,11 @@ return {
   -- Useful plugin to show you pending keybinds.
   "folke/which-key.nvim",
   event = "VimEnter",
-  config = function()
-    require("which-key").setup({})
-
-    -- document existing key chains
-    require("which-key").add({
+  opts = {
+    spec = {
       { "<leader>c", group = "[C]ode" },
       { "<leader>g", group = "[G]it", mode = { "n", "v" } },
       { "<leader>s", group = "[S]earch" },
-    })
-  end,
+    },
+  },
 }
